f3316fb883815fa8fe1f363b7bcb5f216734bb82
vladv333 <vld.vladimirov02@gmail.com>
2026-04-06 21:55:15 +0300

    Initial project setup

---
+++ src/main/java/com/example/guessgame/GuessgameApplication.java
@@ -0,0 +1,13 @@
+package com.example.guessgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guessgame/GuessgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guessgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
